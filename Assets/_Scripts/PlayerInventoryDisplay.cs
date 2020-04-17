583adb541354d62bf43ffa04e05d608172e95c90
ShaunFerns <shaun.ferns@tudublin.ie>
2020-04-17 15:13:29 +0100

    1st commit

---
+++ Assets/_Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerInventory))]
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+    public Image iconStarsYellow;
+
+    public void OnChangeStarTotal(int starTotal)
+    {
+        float newWidth = 100 * starTotal;
+        iconStarsYellow.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
+    }
+}
